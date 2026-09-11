7cdc404cb5d0ae7aed0c675dfc9e1c0faf8f39af
egigoka <egigoka@gmail.com>
2026-09-12 04:18:52 +0500

    release 0.30.0

--- tests/c4_wasm_allocation_contract.mjs
+++ tests/c4_wasm_allocation_contract.mjs
@@ -168,7 +168,7 @@ assert.deepEqual(unpack(dispatch(
   capOperation.length,
   capInput.pointer,
   capInput.length,
-)), { ok: true, value: { schemaVersion: 1, coreVersion: "0.29.0" } });
+)), { ok: true, value: { schemaVersion: 1, coreVersion: "0.30.0" } });
 releaseBuffer(capInput);
 releaseBuffer(capOperation);
 
